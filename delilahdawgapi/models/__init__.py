281d28358bd09dbb241f3a8e44813a16329e0a62
ColinFChevalier <colinfchevalier@gmail.com>
2022-01-24 19:36:33 -0600

    more models added

--- delilahdawgapi/models/__init__.py
+++ delilahdawgapi/models/__init__.py
@@ -1,3 +1,4 @@
 from .post import Post
 from .category import Category
 from .tag import Tag
+from .reaction import Reaction
--- delilahdawgapi/models/post.py
+++ delilahdawgapi/models/post.py
@@ -1,6 +1,7 @@
 from unicodedata import category
 from django.db import models
 from .category import Category
+from .reaction import Reaction
 
 class Post(models.Model):
 
@@ -13,6 +14,10 @@ class Post(models.Model):
         verbose_name="Category",
         null=True,
         on_delete=models.SET_NULL
+    )
+    reaction = models.ForeignKey(
+        Reaction,
+        related_name="post_reaction"
     )
     publication_date = models.DateTimeField()
     image_url = models.URLField(max_length=200)
