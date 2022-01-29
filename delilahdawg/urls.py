9089890f8b1c07253134707a2b7e0a0c2d17f4b8
Big-Red-Jon <jonathan.brooks21@yahoo.com>
2022-01-29 11:27:24 -0600

    Post Views Created

--- delilahdawg/urls.py
+++ delilahdawg/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from delilahdawgapi.views import register_user, login_user
+from rest_framework import routers
+from delilahdawgapi.views import register_user, login_user, PostView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'posts', PostView, 'post')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-    
+
 ]
