7b9ae16d9c9bb36e146d07290bafecb5aa26450d
Stephanie Hamilton <hamiltons621@gmail.com>
2022-02-05 10:40:57 -0600

    updated tag viewset and model to remove post

--- delilahdawgapi/models/tag.py
+++ delilahdawgapi/models/tag.py
@@ -1,15 +1,8 @@
 from django.db import models
-from .post import Post
 
 class Tag(models.Model):
 
     label = models.CharField(
         max_length=20,
         null=True  
-    )
-    post = models.ForeignKey(
-        Post,
-        null=True,
-        related_name="post_tag",
-        on_delete=models.CASCADE
     )
